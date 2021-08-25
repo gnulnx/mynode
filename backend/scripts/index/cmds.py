facb4f88493a892a4dd69387e416b7161c5d8ed4
Your Name <you@example.com>
2021-08-25 12:44:02 -0400

    blocks db, indexing spinner

--- backend/scripts/index/cmds.py
+++ backend/scripts/index/cmds.py
@@ -6,6 +6,9 @@ from server import mongo
 from wallet.core.utils.jprint import jprint
 from models.transaction.model import Transaction
 from datetime import datetime
+from tqdm import tnrange, tqdm
+from tqdm import tqdm
+from alive_progress import alive_bar
 
 
 @index.command()
@@ -33,53 +36,50 @@ def all(start):
     print("height: %s" % height)
     print("start: %s" % start)
 
-    for idx in range(start, height):
-        # Track the unprocessable blocks so we don't have to do it again on each new iteration
-        # if mongo.bitcoin.badblocks.find_one({"blocks": idx}):
-        #     continue
+    with alive_bar(height - start, bar="blocks", spinner="fish2", length=75) as bar:
+        for idx in range(start, height):
+            hash = bitcoin.getblockhash(idx)
+            block = bitcoin.getblock(hash, 1)
+            out = mongo.bitcoin.blocks.update_one(
+                {"idx": idx},
+                {
+                    "$set": {
+                        "nTx": block["nTx"],
+                        "ts": datetime.fromtimestamp(block["time"]),
+                        "hash": hash,
+                    }
+                },
+                upsert=True,
+            )
+            bar()
+            if out.matched_count == 1:
+                # Don't process blocks that have already been processed
+                continue
 
-        hash = bitcoin.getblockhash(idx)
-        block = bitcoin.getblock(hash, 1)
-        out = mongo.bitcoin.blocks.update_one(
-            {"idx": idx},
-            {
-                "$set": {
-                    "nTx": block["nTx"],
-                    "ts": datetime.fromtimestamp(block["time"]),
-                    "hash": hash,
-                }
-            },
-            upsert=True,
-        )
-        if out.matched_count == 1:
-            # Don't process blocks that have already been processed
-            continue
-        ts = datetime.fromtimestamp(block["time"]).strftime("%Y-%m-%d %h:%M:%S")
-        print(
-            f"{idx} of {height}, hash: {block['hash']}, Transaction: {block['nTx']}, ts: {ts}"
-        )
+            # A different type of output
+            # ts = datetime.fromtimestamp(block["time"]).strftime("%Y-%m-%d %h:%M:%S")
+            # print(
+            #     f"{idx} of {height}, hash: {block['hash']}, Transaction: {block['nTx']}, ts: {ts}"
+            # )
 
-        for tx in block["tx"]:
-            # tx = "5933f83f611896b3f35fd650b4f03f9d85d4b6491299c5c5398000834929a224"
-            txn = Transaction(tx)
+            for tx in block["tx"]:
+                # tx = "5933f83f611896b3f35fd650b4f03f9d85d4b6491299c5c5398000834929a224"
+                txn = Transaction(tx)
 
-            for address in txn.outputs:
-                # Update address collection
-                mongo.bitcoin.addresses.update_one(
-                    {"address": address["address"]},
-                    {"$addToSet": {"inputs": tx}},
-                    upsert=True,
-                )
-            for address in txn.inputs:
-                mongo.bitcoin.addresses.update_one(
-                    {"address": address["address"]},
-                    {"$addToSet": {"outputs": tx}},
-                    upsert=True,
-                )
+                for address in txn.outputs:
+                    # Update address collection
+                    mongo.bitcoin.addresses.update_one(
+                        {"address": address["address"]},
+                        {"$addToSet": {"inputs": tx}},
+                        upsert=True,
+                    )
+                for address in txn.inputs:
+                    mongo.bitcoin.addresses.update_one(
+                        {"address": address["address"]},
+                        {"$addToSet": {"outputs": tx}},
+                        upsert=True,
+                    )
 
-            if txn.errors:
-                jprint(txn.errors)
-
-        if idx and idx % 100 == 0:
-            print("\nTotal addresses: %s" % len(addresses))
-            print("Total txns: %s" % len(transactions))
+                if txn.errors:
+                    # TODO Log this...
+                    jprint(txn.errors)
